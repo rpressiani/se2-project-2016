473e6b04af877b6a0e37e14dda911d23460cdde6
MatteoPenco <matteo.penco05gmail.com>
2016-12-09 16:26:02 +0100

    added algorithms

---
+++ DD/resources/Algorithms/Rental.java
@@ -0,0 +1,30 @@
+package sw2;
+
+public class Rental {
+	
+	private Car car;
+	private int duration;
+	private double amountToPay;
+	
+	public Rental(Car car, int n, double p){
+		this.car=car;
+		this.duration=n;
+		this.amountToPay=p;
+	}
+	
+	public Car getCar() {
+		return car;
+	}
+	
+	public int getDuration() {
+		return duration;
+	}
+	
+	public double getAmountToPay() {
+		return amountToPay;
+	}
+	
+	public void setAmountToPay(double amountToPay) {
+		this.amountToPay=amountToPay;
+	}
+}
