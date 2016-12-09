ed858d9c2fb704ba879be89dc89230498e009068
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2016-12-09 18:15:50 +0100

    Add algorithm to DD latex document

--- DD/resources/Algorithms/Position.java
+++ DD/resources/Algorithms/Position.java
@@ -1,37 +1,43 @@
 package sw2;
 
 public class Position {
-	private float latitude;
-	private float longitude;
-	
-	public Position(int x, int y){
-		this.latitude=x;
-		this.longitude=y;
-	}
-	
-	public float getLatitude() {
-		return latitude;
-	}
-	
-	public float getLongitude() {
-		return longitude;
-	}
+  private float latitude;
+  private float longitude;
+  
+  public Position(int x, int y){
+    this.latitude=x;
+    this.longitude=y;
+  }
+  
+  public float getLatitude() {
+    return latitude;
+  }
+  
+  public float getLongitude() {
+    return longitude;
+  }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Position other = (Position) obj;
-		if (Float.floatToIntBits(latitude) != Float.floatToIntBits(other.latitude))
-			return false;
-		if (Float.floatToIntBits(longitude) != Float.floatToIntBits(other.longitude))
-			return false;
-		return true;
-	}
-	
-	
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Position other = (Position) obj;
+    if (
+      Float.floatToIntBits(latitude) !=
+      Float.floatToIntBits(other.latitude)
+      )
+      return false;
+    if (
+      Float.floatToIntBits(longitude) !=
+      Float.floatToIntBits(other.longitude)
+      )
+      return false;
+    return true;
+  }
+  
+  
 }
--- DD/resources/Algorithms/SafeArea.java
+++ DD/resources/Algorithms/SafeArea.java
@@ -2,20 +2,20 @@ package sw2;
 
 public class SafeArea {
 
-	private Position position;
-	private boolean powerGridPresence;
-	
-	public SafeArea(Position position, boolean powerGridPresence){
-		this.position=position;
-		this.powerGridPresence=powerGridPresence;
-	}
-	
-	public Position getPosition() {
-		return position;
-	}
+  private Position position;
+  private boolean powerGridPresence;
+  
+  public SafeArea(Position position, boolean powerGridPresence){
+    this.position=position;
+    this.powerGridPresence=powerGridPresence;
+  }
+  
+  public Position getPosition() {
+    return position;
+  }
 
-	public boolean isPowerGridPresence() {
-		return powerGridPresence;
-	}
-	
+  public boolean isPowerGridPresence() {
+    return powerGridPresence;
+  }
+  
 }
